68cf5d77ae6c7ec58f14cd951000848ab1858b50
KatieJessupMcD <katie.jessupmcd@gmail.com>
2022-03-12 15:02:53 -0800

    first pass of basic query view of students in each school, will parse out further, this is mostly just a proof of concept. TODO try out a proof of concept utilizing mutation from backend to create and add a new student to an existing school

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+# 🎼 Band Program Content Management System - Frontend
+
 Frontend for band program content management system. Build with React, Apollo, GraphQL
--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import { useQuery, gql } from '@apollo/client';
 
-const GET_SCHOOLS = gql`
+const GET_SCHOOLS_AND_STUDENTS = gql`
   query getSchools {
     schools {
       name
+      students {
+        firstName
+        lastName
+      }
     }
   }
 `;
 
 function App() {
-  const { loading, error, data } = useQuery(GET_SCHOOLS);
+  const { loading, error, data } = useQuery(GET_SCHOOLS_AND_STUDENTS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
-  return data.schools.map(({ id, name }) => (
+  return data.schools.map(({ id, name, students }) => (
     <div key={id}>
-      <p>{name}</p>
+      <h2>{name}</h2>
+      {students.map((student) => (
+        <ul>
+          <li>
+            {student.firstName} {student.lastName}
+          </li>
+        </ul>
+      ))}
     </div>
   ));
 }
